9b238fff878a61e484045139efa49e7bfa370822
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-02 02:20:47 +0100

    ex2

--- exemple2.js
+++ exemple2.js
@@ -4,9 +4,7 @@ let person1 = {
         console.log(this.fullname + " says: " +  str);
     }
 };
-//person1.say("hello")
+person1.say("hello")
 var person2 = {
     fullname: 'Allani Ahmed'
 }
-var say2 = person1.say.bind(person2,"hi")
-say2()
